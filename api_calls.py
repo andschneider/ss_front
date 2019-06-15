114019240a3a3a6873d1c075662ab792ce0a181e
Andrew Schneider <aschne93@gmail.com>
2019-06-15 10:53:37 -0700

    feat: Update for new api
    - fully switched to non-serverless api

--- api_calls.py
+++ api_calls.py
@@ -1,58 +1,47 @@
-from configparser import ConfigParser
 import os
 
 import requests
 
-from locations import SETTINGS_FOLDER
-
 
 class SensorAPI:
     def __init__(self):
-        self.base_url, self.api_version = self._load_api_settings()
+        self.base_url = os.getenv("API_URL", "http://localhost:3030")
+        self.api_prefix = "/api/v1"
+        self._token = self._authenticate()
 
-    @staticmethod
-    def _load_api_settings():
-        parser = ConfigParser()
-        settings_file = os.path.join(SETTINGS_FOLDER, "api_settings.ini")
-        if os.path.exists(settings_file):
-            parser.read(settings_file)
-            url = parser["web"]["base_url"]
-            return url, 1
-        else:
-            url = "http://localhost:3030"
-            print(f"No API settings file found. Defaulting to {url}")
-            return url, 2
+    def _authenticate(self):
+        username = os.getenv("API_USERNAME")
+        password = os.getenv("API_PASSWORD")
+        url = self.base_url + self.api_prefix + "/auth"
+
+        response = requests.post(url, json={"username": username, "password": password})
+
+        if response.status_code != 200:
+            raise ConnectionRefusedError("Could not authenticate with API.")
+
+        return "Bearer " + response.json()["auth_token"]
 
     def get_sensor_ids(self):
-        if self.api_version == 1:
-            url = self.base_url + "/get_sensor_ids"
-        else:
-            url = self.base_url + "/sensor_ids"
-        r = requests.get(url)
+        url = self.base_url + self.api_prefix + "/sensor_ids"
+        r = requests.get(url, headers={"Authorization": self._token})
         return r.status_code, r.json()
 
     def get_sensor_data(self, minutes, sensor_ids):
-        if self.api_version == 1:
-            url = self.base_url + "/get_sensor_data"
-        else:
-            url = self.base_url + "/sensor_data"
+        url = self.base_url + self.api_prefix + "/sensor_data"
 
         id_string = ",".join([str(s_id) for s_id in sensor_ids])
         args = f"sensor_ids={id_string}&minutes={minutes}"
 
         full_url = url + "?" + args
-        r = requests.get(full_url)
+        r = requests.get(full_url, headers={"Authorization": self._token})
+
         if r.status_code == 200:
             return r.json()
         # TODO catch api errors
         # return r.status_code, r.json()
 
     def get_plant_by_id(self, sensor_id):
-        if self.api_version == 1:
-            url = self.base_url + "/sensor_info"
-            r = requests.get(url, params={"sensor_id": sensor_id})
-        else:
-            url = self.base_url + "/sensor_info/" + str(sensor_id)
-            r = requests.get(url)
+        url = self.base_url + self.api_prefix + "/sensor_info/" + str(sensor_id)
+        r = requests.get(url, headers={"Authorization": self._token})
 
         return r.status_code, r.json()
